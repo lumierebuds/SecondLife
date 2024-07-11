4aa25f4b7a348a3ade5abe420619032ef6255601
lumierebuds <lumierebuds@gmail.com>
2024-07-11 16:32:17 +0900

    게시판 기능 import 구문 수정

--- src/main/java/com/kh/secondLife/board/model/dao/BoardDaoImpl.java
+++ src/main/java/com/kh/secondLife/board/model/dao/BoardDaoImpl.java
@@ -14,6 +14,7 @@ import org.apache.ibatis.session.SqlSession;
 import org.springframework.stereotype.Repository;
 
 import com.kh.secondLife.board.model.vo.Board;
+import com.kh.secondLife.board.model.vo.BoardImg;
 import com.kh.secondLife.common.model.vo.PageInfo;
 
 import lombok.RequiredArgsConstructor;
